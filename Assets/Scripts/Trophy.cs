8e4998ada954cf228fe2efb74e0c3333c555473e
Jeanne Beyazian <jeanne.beyazian@kcl.ac.uk>
2022-03-14 13:51:07 +0000

    Change version

---
+++ Assets/Scripts/Trophy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ubiq.XR;
+
+public class Trophy : MonoBehaviour, IGraspable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+    public void Grasp(Hand hand)
+    {
+
+        Debug.Log("Grasped");
+
+    }
+    
+    public void Release(Hand hand)
+    {
+
+        Debug.Log("Grasped");
+
+    }
+    
+}
