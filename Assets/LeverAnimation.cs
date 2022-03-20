fd5c67228afdd57d7d21e08e53a9ca472d79c60a
k1888763 <jeanne.beyazian@kcl.ac.uk>
2022-03-20 14:16:45 +0000

    Set up lever animation

---
+++ Assets/LeverAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverAnimation : MonoBehaviour
+
+{
+
+    public Animator anim;
+    public bool isOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        isOn = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f")) {
+
+            if (isOn) {
+                anim.Play("Off Lever"); 
+                isOn = false;
+            }
+            else {
+                anim.Play("On Lever");
+                isOn = true;
+            }
+        
+        }
+              
+    }
+}
